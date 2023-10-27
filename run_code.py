7ece7f43c69a7d789c00ca951e8796d8cbaee0a6
rainrajon <147498777+rainrajon@users.noreply.github.com>
2023-10-27 11:21:33 +0530

    Create run_code.py
    made demo changes

---
+++ run_code.py
@@ -0,0 +1,10 @@
+def factorial(n):
+  if n==0:
+    return 1
+  else:
+    return n*factorial(n-1)
+
+num=5
+fact_result=factorial(num)
+print(f"The factorial of {num} is {fact_result}")
+print("Hello prajwal you can show this to amol and cithravel!!!!")
